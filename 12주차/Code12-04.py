a411316e26398cccd815c10923df60e2dcda7c3d
yoonyijin <dlwls3415@naver.com>
2025-05-23 14:06:25 +0900

    Add files via upload

---
+++ 12주차/Code12-04.py
@@ -0,0 +1,10 @@
+class Car :
+    color = ""
+    speed = 0
+
+    def __init__(self, value1, value2) :
+        self.color = value1
+        self.speed = value2
+    
+myCar1 = Car("빨강", 30)
+myCar2 = Car("파랑", 60)
